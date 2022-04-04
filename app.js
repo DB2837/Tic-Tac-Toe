05fa044af25144b536cad10921aab18d77483c82
davide <davidebellucci975@gmail.com>
2022-04-04 11:26:11 +0200

    add logic to handle turns

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ class Tile {
   constructor(signValue = "") {
     this.sign = signValue;
     this.coordinateNumber = ++Tile.counter;
-    /* console.log(this.coordinateNumber); */
   }
 
   getCoordinateNumber() {
@@ -27,6 +26,7 @@ class Tile {
 
 class Player {
   playerScore = 0;
+  isMyTurn = true;
 
   constructor(sign) {
     this.signValue = sign;
@@ -72,17 +72,52 @@ class GameBoard {
   }
 }
 
-class UI {}
+class UI {
+  static startGame() {} //add eventlistener on click to start the game (create board and players)
+
+  static checkTileContent(gameBoard, coordinateNumber) {
+    return gameBoard.getTile(coordinateNumber).getSignValue();
+  }
+
+  static handleTurns(playerOne, playerTwo) {
+    if (playerOne.isMyTurn == true) {
+      playerOne.isMyTurn = false;
+      playerTwo.isMyTurn = true;
+      return playerOne;
+    } else if (playerOne.isMyTurn == false) {
+      playerOne.isMyTurn = true;
+      playerTwo.isMyTurn = false;
+      return playerTwo;
+    }
+  }
+
+  static renderSign(target, player, gameBoard) {
+    if (UI.checkTileContent(gameBoard, target.dataset.coordinate)) return;
+
+    const tile = document.querySelector(
+      `[data-coordinate="${target.dataset.coordinate}"]`
+    );
+    player.drawSign(target.dataset.coordinate, gameBoard);
+
+    const sign = document.createTextNode(player.getSign());
+
+    tile.appendChild(sign);
+    board.displayTiles();
+  }
+
+  static checkForWinner(gameBoard) {}
+}
 
 const board = new GameBoard();
 board.inizializeGameBoard();
-
 const playerOne = new Player(Sign.X);
 const playerTwo = new Player(Sign.O);
 
-playerOne.drawSign(1, board);
-playerTwo.drawSign(6, board);
-playerOne.drawSign(4, board);
-playerTwo.drawSign(4, board);
+/* playerOne.isMyTurn = false; */
 
-board.displayTiles();
+const tiles = document.querySelector(".grid");
+tiles.addEventListener("click", (e) => {
+  if (UI.checkTileContent(board, e.target.dataset.coordinate)) return; //if not "" return;
+  UI.renderSign(e.target, UI.handleTurns(playerOne, playerTwo), board);
+  UI.checkForWinner(board);
+});
